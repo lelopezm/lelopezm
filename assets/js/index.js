da24322b08940d224b6286b9aa3d7fe878e93cbd
TheZombie272 <uzumakyinuyasha@gmail.com>
2024-11-06 15:44:04 -0500

    Que pena por la demora profe, es que no me acordaba que tenía que subi el cambio a este repositorio

--- assets/js/index.js
+++ assets/js/index.js
@@ -14,7 +14,28 @@ function cargarContenidoHTML(ruta, elementoID) {
         .catch(error => console.error('Error:', error));  // Manejamos errores.
 }
 
+function cargarLinkContent(ruta, elementoID){
+    fetch(ruta)  // Hacemos la solicitud al archivo HTML.
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error al cargar el archivo HTML');
+        }
+        return response.text();  // Extraemos el texto (contenido HTML).
+    })
+    .then(data => {
+        // Insertamos el contenido en el elemento con el ID especificado.
+        document.getElementById(elementoID).addEventListener( "click", ()=>{
+            cargarContenidoHTML(ruta, 'index-content');
+        })
+    })
+    .catch(error => console.error('Error:', error));  // Manejamos errores.
+}
+
 // Cargar el contenido HTML en el div con id="contenido"
 cargarContenidoHTML('assets/html/header.html', 'index-header');
 cargarContenidoHTML('assets/html/index-content.html', 'index-content');
 cargarContenidoHTML('assets/html/footer.html', 'index-footer');
+
+// Si desea cargar un documento html en el content del index, solo es que agregue la ruta del html y luego
+// poner el id del link( el id va en <a id= " ">), recordar quitarle al link ( target="_blanck" ) a la etiqueta "a"
+cargarLinkContent('assets/html/fundamentales.html','link_fundamentales')
